50e9c42a8b3642e31529313facebf2a2fa8bb0e9
StanleyPangaruy <pangaruystanley@gmail.com>
2021-11-14 17:33:54 +0800

    added functions to allow users to input a float number and integer number at the same time

--- Find_the_lowest_number.py
+++ Find_the_lowest_number.py
@@ -3,15 +3,44 @@
 # Find the lowest number using only if-else statement.
 # Display the lowest number
 
-def lowestNumber():
-    num1_ = int(input("First number: "))
-    num2_ = int(input("Second number: "))
-    num3_ = int(input("Third number: "))
-    if num1_ < num2_ and num1_ < num3_:
-        print(num1_)
-    elif num2_ < num1_ and num2_ < num3_:
-        print(num2_)
+def is_int(n):
+    try:
+        floatN = float(n)
+        intN = int(floatN)
+    except ValueError:
+        return False
     else:
-        print(num3_)
+        return floatN == intN
+
+def is_float(n):
+    try:
+        floatN = float(n)
+    except ValueError:
+        return False
+    else:
+        return True
+
+def lowestNumber():
+    num1 = input("First number: ")
+    if is_int(num1):
+        num1 = int(num1)
+    elif is_float(num1):
+        num1 = float(num1)
+    num2 = input("Second number: ")
+    if is_int(num2):
+        num2 = int(num2)
+    elif is_float(num2):
+        num2 = float(num2)
+    num3 = input("Third number: ")
+    if is_int(num3):
+        num3 = int(num3)
+    elif is_float(num2):
+        num3 = float(num3) 
+    if num1 < num2 and num1 < num3:
+        print("Lowest Number:",num1)
+    elif num2 < num1 and num2 < num3:
+        print("Lowest Number:",num2)
+    else:
+        print("Lowest Number:",num3)
 
 lowestNumber()
